7d274412c6d376af88098eaec16fe1573d6027c8
Med Elmostapha <dedijekani@gmail.com>
2026-04-21 20:13:05 +0000

    chore: building note model

---
+++ lib/data/note_reposetry.dart
@@ -0,0 +1,24 @@
+// import 'package:bloc_notes/models/note.dart';
+
+// class NoteReposetry {
+//   final _notes = [
+//     Note(
+//       id: '1',
+//       titre: 'Shopping',
+//       contenu: 'Buy milk and eggs',
+//       couleur: '#FFE082',
+//       dateCreation: DateTime.now(),
+//     ),
+//     Note(
+//       id: '2',
+//       titre: 'Project',
+//       contenu: 'Finish Flutter TP',
+//       couleur: '#FFAB91',
+//       dateCreation: DateTime.now(),
+//     ),
+//   ];
+
+//   List<Note> getAllNotes() {
+//     return _notes;
+//   }
+// }
